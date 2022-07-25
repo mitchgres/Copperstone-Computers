e8afb4cec1ab903c67c0dc07ad3cd82c232104c5
Mitchell Gresham <95192428+mitchgres@users.noreply.github.com>
2022-07-25 17:11:29 -0400

    Initial Commit
    Includes Initial Commit for Home Page & About Page

---
+++ front-end-js.js
@@ -0,0 +1,10 @@
+if (document.URL.includes("index")) {
+    document.getElementById("home-link").style.color = '#d8d8d8';
+    document.getElementById("home-link").style.borderBottomColor = "#d8d8d8";
+    document.getElementById("home-link").style.borderBottomWidth = "4px";
+}
+if (document.URL.includes("about")) {
+    document.getElementById("about-link").style.color = '#d8d8d8';
+    document.getElementById("about-link").style.borderBottomColor = "#d8d8d8";
+    document.getElementById("about-link").style.borderBottomWidth = "4px";
+}
