f9e83517ecddc78031e2434328f2a58c4827e5b5
Mitchell Gresham <95192428+mitchgres@users.noreply.github.com>
2022-07-25 21:35:50 -0400

    Added Headers & Professional Page
    - Added Headers To Contact Us & E-Sports
    - Added Professional Page
    - Reorganized Images On Webpage
    - Better File Organization

---
+++ js/front-end-js.js
@@ -0,0 +1,21 @@
+if (document.URL.includes("index")) {
+    document.getElementById("home-link").style.color = '#d8d8d8';
+    document.getElementById("home-link").style.borderBottomColor = "#d8d8d8";
+    document.getElementById("home-link").style.borderBottomWidth = "4px";
+} else if (document.URL.includes("about")) {
+    document.getElementById("about-link").style.color = '#d8d8d8';
+    document.getElementById("about-link").style.borderBottomColor = "#d8d8d8";
+    document.getElementById("about-link").style.borderBottomWidth = "4px";
+} else if (document.URL.includes("professional")) {
+    document.getElementById("professional-link").style.color = '#d8d8d8';
+    document.getElementById("professional-link").style.borderBottomColor = "#d8d8d8";
+    document.getElementById("professional-link").style.borderBottomWidth = "4px";
+} else if (document.URL.includes("sports")) {
+    document.getElementById("esport-link").style.color = '#d8d8d8';
+    document.getElementById("esport-link").style.borderBottomColor = "#d8d8d8";
+    document.getElementById("esport-link").style.borderBottomWidth = "4px";
+} else if (document.URL.includes("contact")) {
+    document.getElementById("contact-link").style.color = '#d8d8d8';
+    document.getElementById("contact-link").style.borderBottomColor = "#d8d8d8";
+    document.getElementById("contact-link").style.borderBottomWidth = "4px";
+}
